f038853fcf778edef472e72dbbb85957b3a38424
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-03-22 01:33:58 +0530

    22nd_March

---
+++ lovebabbar/array/23_Majority_Elements.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+   vector<int> arr = {1,2,1,1,1,3,3,3,4,3,3,1};
+
+   int n = arr.size();
+
+   int majority = ceil(n/3);
+
+   
+ 
+   return 0;
+}
