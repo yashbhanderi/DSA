f9cfec6ab6375f2f1ecfa7cadf5a0da328cd6890
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-04-14 01:46:50 +0530

    14th_April

---
+++ lovebabbar/linked_list/1_reverse_linked_list.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "linked_list.cpp"
+
+using namespace std;
+
+int main() {
+
+    LinkedList ll;
+
+    ll.insertLast(10);
+    ll.insertLast(20);
+    ll.insertLast(30);
+
+    return 0;
+}
