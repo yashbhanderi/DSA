1732fed9853cae48ff988b2e6cbf0aafb2d66b29
Yash Bhanderi <yashbhanderi1818@gmail.com>
2023-10-01 00:07:00 +0530

    added two problems in array

---
+++ striver/SDE Sheet - 450/Array/3_remove_duplicates_from_sorted_array_in-place.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> arr = {3, 4, 5, 1, 2};
+
+    map<int, int> mp;
+
+    for (auto it : arr) mp[it]++;
+
+    auto itr = mp.begin();
+
+    for (int i = 0; i < mp.size(); i++) {
+        arr[i] = itr->first;
+        itr++;
+    }
+
+    return mp.size();
+
+    return 0;
+}
--- striver/SDE Sheet - 450/Maths/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-
-    if(b==0) return a;
-
-    return GCD(b, a-b);
